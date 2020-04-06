af0972bb36d224a6d1aa7eb7f56564c364ad736d
Rusca <rusca.dev@gmail.com>
2020-04-06 19:43:48 +0200

    evita error de token no vàlid

--- deuresweb/telegramor.py
+++ deuresweb/telegramor.py
@@ -8,4 +8,7 @@ def feedback(tema, opcions):
     telegramText = "S'han generat exercicis *" + tematitol(tema) + "* _(" + opcions["curs"] + ")_ "
     telegramText = urllib.parse.quote_plus(telegramText)
     botToken = gettoken()
-    urllib.request.urlopen("https://api.telegram.org/bot"+ botToken + "/sendMessage?chat_id=@ddfeedback&disable_notification=true&text="+ telegramText +"&parse_mode=Markdown").read()
+    if gettoken() == "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11":
+        print("utilitzant el token de prova")
+    else:
+        urllib.request.urlopen("https://api.telegram.org/bot"+ botToken + "/sendMessage?chat_id=@ddfeedback&disable_notification=true&text="+ telegramText +"&parse_mode=Markdown").read()
