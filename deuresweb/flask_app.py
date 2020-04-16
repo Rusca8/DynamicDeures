6d2d6f5ce6f9541de56a122597fb4eac41646503
Rusca <rusca.dev@gmail.com>
2020-04-16 22:20:42 +0200

    una mica de try catch

--- deuresweb/flask_app.py
+++ deuresweb/flask_app.py
@@ -12,12 +12,19 @@ app = Flask(__name__, template_folder="templates/")
 
 @app.route('/')
 def index():
-    return render_template("index.html", ex=e.genera())
+    try:
+        return render_template("index.html", ex=e.genera())
+    except:
+        return f"<h1>Ui</h1><h2>Hi ha hagut algun problema carregant la pàgina.</h2><p>Si es manté avisa'm! (Twitter: @Rusca8 | Insta: @drusca8 | Mail: rusca.dev@gmail.com)</p>"
 
 
 @app.route('/ex/')
 def indexvar():
-    return render_template("index.html", ex=e.genera(True))
+    try:
+        return render_template("index.html", ex=e.genera(True))
+    except:
+        return f"<h1>Ui</h1><h2>Hi ha hagut algun problema carregant la pàgina.</h2><p>Si es manté avisa'm! (Twitter: @Rusca8 | Insta: @drusca8 | Mail: rusca.dev@gmail.com)</p>"
+
 
 
 @app.route("/equacions/", methods=["GET", "POST"])
