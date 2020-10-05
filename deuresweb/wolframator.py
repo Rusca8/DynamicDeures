d2164a7f624114c8edc52dcbd9ac2fd62c98bf34
Rusca <rusca.dev@gmail.com>
2020-10-05 20:38:12 +0200

    millores links wolfram i els activo per defecte

--- deuresweb/wolframator.py
+++ deuresweb/wolframator.py
@@ -3,6 +3,7 @@ from urllib.parse import quote_plus as quote
 
 def urlfor(latex, t=0):
     url = "https://www.wolframalpha.com/input/?i="
+    latex = latex.replace(r"\,", "").replace(r"\frac{1}{2}", "{1/2}")
     if t == "dx":
         latex = "derivative of " + latex
     url += quote(latex)
